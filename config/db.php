219306235bf01b720ea9a32682040b9d49aedcd8
Nikolay Kostyurin <jilizart@gmail.com>
2014-12-15 09:05:42 +0700

    initial

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2-qa-demo',
+    'username' => 'yii2-qa-demo',
+    'password' => 'yii2-qa-demo',
+    'charset' => 'utf8',
+];
---
+++ views/layouts/main.php
@@ -0,0 +1,72 @@
+<?php
+use app\assets\AppAsset;
+use artkost\qa\widgets\Favorite;
+use artkost\qa\widgets\Popular;
+use artkost\qa\widgets\Tags;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\helpers\Html;
+use yii\widgets\Breadcrumbs;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+
+<?php $this->beginBody() ?>
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => 'Questions and Answers',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'nav navbar-nav navbar-right'],
+        'items' => [
+            ['label' => 'Home', 'url' => ['/site/index']],
+            Yii::$app->user->isGuest ?
+                ['label' => 'Login', 'url' => ['/user/security/login']] :
+                ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+                    'url' => ['/user/security/logout'],
+                    'linkOptions' => ['data-method' => 'post']],
+        ],
+    ]);
+    NavBar::end();
+    ?>
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <div class="row">
+            <div class="col-md-9">
+                <?= $content ?>
+            </div>
+            <div class="col-md-3">
+                <?= Tags::widget() ?>
+                <?= Popular::widget() ?>
+                <?= Favorite::widget(['userID' => Yii::$app->user->id]) ?>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
